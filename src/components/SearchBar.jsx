3f2885716153aa194b186d1f926b23aec2720aca
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-17 15:01:02 +0100

    feat: Ajouter le contenu de frontend de searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,12 @@
-export default function SearchBar(){
+export default function SearchBar() {
 
     return (
-        <div>
-
+        <div className="bg-slate-800 flex flex-grow justify-center">
+            <h1 className="text-green-900 font-bold text-5xl">MUSEEKLY</h1>
+            <div >
+                <input type="text" placeholder="Search ..." />
+                <button className="bg-green-200">Search</button>
+            </div>
         </div>
     );
 }
