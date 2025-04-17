e36c423205e4cd9a24ebb1037e3666bd2ff6c3e1
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-17 17:27:40 +0100

    refactor: réorganisation complète de la structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import SearchBar from './components/SearchBar';
-
+import SearchBar from "./components/SearchBar";
 export default function App() {
-
   return (
-    <>
-        <div>
-      <header>
-
+    <div className="min-h-screen flex flex-col">
+      <header className="flex-grow-0">
+        <SearchBar />
       </header>
-      <main>
-        <div>
-          <SearchBar />
-        </div>
-        <div>
-          <div></div>
-          <div></div>
-        </div>
+      
+      <main className="flex-grow">
+        {/* Autres sections iront ici */}
       </main>
-      <footer className="flex justify-center">
+      
+      <footer className="flex justify-center py-4 bg-slate-800 text-white">
         <p>Museekly &copy; 2025</p>
       </footer>
-    </div></>
-  )
+    </div>
+  );
 }
