4d6798c927a8a77cb14769332271c5516d7e985a
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-17 17:26:26 +0100

    feat: Ajout de la gestion d'état et validation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 export default function SearchBar() {
+    const [searchInput, setSearchInput] = useState('');
+    const [error, setError] = useState('');
+
+    const fetchdata = async (e) => {
+        e.preventDefault();
+
+        if (!searchInput.trim()) {
+            setError('Search field is empty');
+            return;
+        }
+
+        console.log(searchInput);
+    }
 
     return (
         <div className="bg-slate-800 flex flex-col items-center pt-4 px-6">
             <h1 className="text-green-400 mb-10 font-bold text-5xl tracking-widest">MUSEEKLY</h1>
-            <div className="flex justify-center max-w-md w-full">
-                <input type="text" placeholder="Search ..." className="px-4 py-2 rounded-l-full flex-grow focus:ring-2 focus:ring-green-400 focus:outline-none"/>
-                <button className="font-bold text-slate-900 bg-green-300 hover:bg-green-600 rounded-r-full px-4 py-2 transition-colors duration-300">Search</button>
-            </div>
+            {error && <div className="text-red-500 mb-1">{error}</div>}
+            <form onSubmit={fetchdata} className="flex justify-center max-w-md w-full mb-4">
+                <input 
+                type="text" 
+                placeholder="Search ..." 
+                className="px-4 py-2 rounded-l-full flex-grow focus:ring-2 focus:ring-green-400 focus:outline-none" 
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)} />
+                <button type="submit"
+                    className="font-bold text-slate-900 bg-green-300 hover:bg-green-600 rounded-r-full px-4 py-2 transition-colors duration-300">Search</button>
+            </form>
         </div>
     );
 }
