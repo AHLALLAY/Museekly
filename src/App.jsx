813509d86f0ca6015233118521ceac38454d2102
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-04-17 15:03:39 +0100

    feat: Ajouter la structure générale du 'App.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,23 @@ import SearchBar from './components/SearchBar';
 export default function App() {
 
   return (
-    <></>
+    <>
+        <div>
+      <header>
+
+      </header>
+      <main>
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </main>
+      <footer className="flex justify-center">
+        <p>Museekly &copy; 2025</p>
+      </footer>
+    </div></>
   )
 }
